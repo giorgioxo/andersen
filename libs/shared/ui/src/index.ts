2e69fa31898ae1a029e3c1023ac4a02f231581fc
Giorgi Khokhonishvili <g.khokhonishvili@andersen-technology.by>
2026-07-06 16:36:13 +0400

    feature: [NO_TASK] implement password visibility toggle

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/spinner/spinner.component';
 export * from './lib/notification/notification.component';
 export * from './lib/notification/notification.service';
 export * from './lib/input/input.component';
+export * from './lib/input/input.model';
+export * from './lib/input/password-visibility.config';
---
+++ libs/shared/ui/src/lib/input/password-visibility.config.ts
@@ -0,0 +1,41 @@
+import {
+  UiInputType,
+  UiPasswordVisibilityConfig,
+  UiPasswordVisibilityIconType,
+  UiPasswordVisibilityState,
+} from './input.model';
+
+export const PASSWORD_VISIBILITY_SVG_ICON_PATH = {
+  Lock: '/assets/icons/lock.svg',
+  Unlock: '/assets/icons/unlock.svg',
+} as const;
+
+export const DEFAULT_PASSWORD_VISIBILITY_CONFIG: UiPasswordVisibilityConfig = {
+  [UiPasswordVisibilityState.Hidden]: {
+    type: UiInputType.Password,
+    icon: 'visibility_off',
+    iconType: UiPasswordVisibilityIconType.Material,
+    ariaLabel: 'Show password',
+  },
+  [UiPasswordVisibilityState.Visible]: {
+    type: UiInputType.Text,
+    icon: 'visibility',
+    iconType: UiPasswordVisibilityIconType.Material,
+    ariaLabel: 'Hide password',
+  },
+};
+
+export const LOCK_PASSWORD_VISIBILITY_CONFIG: UiPasswordVisibilityConfig = {
+  [UiPasswordVisibilityState.Hidden]: {
+    type: UiInputType.Password,
+    icon: PASSWORD_VISIBILITY_SVG_ICON_PATH.Lock,
+    iconType: UiPasswordVisibilityIconType.Svg,
+    ariaLabel: 'Show password',
+  },
+  [UiPasswordVisibilityState.Visible]: {
+    type: UiInputType.Text,
+    icon: PASSWORD_VISIBILITY_SVG_ICON_PATH.Unlock,
+    iconType: UiPasswordVisibilityIconType.Svg,
+    ariaLabel: 'Hide password',
+  },
+};
